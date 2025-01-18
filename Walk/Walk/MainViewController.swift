725315f3f9d275606dfbc5560e1a3d090e8faaf3
jwl-98 <apple_komputer@icloud.com>
2025-01-18 15:22:45 +0900

    initial Commit - 단순 프로젝트 셋팅 파일 추가

---
+++ Walk/Walk/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walk
+//
+//  Created by 진욱의 Macintosh on 1/18/25.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
